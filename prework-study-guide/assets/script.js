5d822ffe957189e6c6ecb1fd3795d2bb48eb27f4
Buster <yahyemohamed2002@gmail.com>
2024-09-14 00:09:56 -0500

    added javascript to make webpage more interactive

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,22 +1,32 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
+function listTopics() {
+  for (let x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+  }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
-  */
+
+}
 
 const shapes = ["triangle", "square", "pentagon", "circle"];
 console.log(shapes[0]);
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
